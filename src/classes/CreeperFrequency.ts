ab8bc525a4ba09c1d3577fb7f01a5e52fd672e8d
James Garner <j@jada.io>
2016-10-05 01:22:06 +0100

    Frequency labels make more sense.

--- src/classes/CreeperFrequency.ts
+++ src/classes/CreeperFrequency.ts
@@ -9,11 +9,11 @@ class CreeperFrequency {
   toString (): string {
     switch (this.value) {
       case 1:
-        return "High (1: 59/60)";
+        return "Low (1/60)";
       case 30:
-        return "Normal (30: 30/60)";
+        return "Normal (30/60)";
       case 59:
-        return "Low (59: 1/60)";
+        return "High (59/60)";
     }
     return "Unknown";
   }
