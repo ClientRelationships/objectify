dbec7b89feb370cd829ecb0d201ac8e0739a9787
James Garner <j@jada.io>
2016-10-08 19:57:48 +0100

    CreeperKeywords upgrade.

--- src/classes/CreeperKeywords.ts
+++ src/classes/CreeperKeywords.ts
@@ -2,41 +2,54 @@ import CreeperActionType = require("./CreeperActionType");
 
 class CreeperKeywords extends Array<string> {
 
-  add (keyword?: string): void {
+  add (keyword?: string): CreeperKeywords {
     if (typeof keyword === "string") {
       this.push(keyword);
     } else {
       this.push(`keyword ${this.length + 1}`);
     }
+    return this;
   }
 
-  update (oldKeyword: string, newKeyword: string): void {
+  updateByIndex (index: number, newKeyword: string): CreeperKeywords {
+    this[index] = newKeyword;
+    return this;
+  }
+
+  update (oldKeyword: string, newKeyword: string): CreeperKeywords {
     let index = this.indexOf(oldKeyword);
-    if (index > -1) {
-      this[index] = newKeyword;
-    }
+    if (!index === -1) return this;
+    return this.updateByIndex(index, newKeyword);
   }
 
-  remove (keyword: string): void {
+  removeByIndex (index: number): CreeperKeywords {
+    console.log(index);
+    this.splice(index, 1);
+    return this;
+  }
+
+  remove (keyword: string): CreeperKeywords {
     let index = this.indexOf(keyword);
-    if (index > -1) {
-      this.splice(index, 1);
-    }
+    if (!index === -1) return this;
+    return this.removeByIndex(index);
   }
 
   toString (): string {
     return this.join(", ");
   }
 
-  fromArray (array: Array<string>): void {
+  fromArray (array: Array<string>): CreeperKeywords {
     this.length = 0;
     array.forEach((keyword) => {
       this.push(keyword);
     });
+    return this;
   }
 
-  fromString (string: string): void {
+  fromString (string: string): CreeperKeywords {
+    if (string.length === 0) return this.fromArray([]);
     this.fromArray(string.split(", "));
+    return this;
   }
 
 }
